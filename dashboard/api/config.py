86ac9a3a4f6d6c75e21931e31740c052afb72cc4
Ales Krenek <ljocha@ics.muni.cz>
2025-03-05 19:57:41 +0100

    data in /mddash

--- dashboard/api/config.py
+++ dashboard/api/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 JUPYTER_USER = os.environ.get('JUPYTERHUB_USER', None)
 PREFIX = f"/user/{JUPYTER_USER}" if JUPYTER_USER else ""
 
-DATA_DIR = Path("/home/jovyan/work/data")
+DATA_DIR = Path("/mddash/db")
 STATE_FILE = DATA_DIR / "experiments.json"
 
 API_DIR = Path(__file__).resolve().parent
