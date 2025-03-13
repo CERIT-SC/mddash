b140e09a01f1f69ece050960d07da9627e4de65f
Filip Krása <xkrasa@fi.muni.cz>
2025-03-13 12:42:26 +0100

    improve wizard UI

--- dashboard/dash/src/components/Wizard/Setup.tsx
+++ dashboard/dash/src/components/Wizard/Setup.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Stack, Button, Typography, CircularProgress } from '@mui/material'
 
 import { WizardStepperProps } from "./Stepper"
 import { get_notebook, spawn_notebook, delete_notebook } from '../../util/api';
@@ -7,24 +7,29 @@ import { get_notebook, spawn_notebook, delete_notebook } from '../../util/api';
 
 const WizardSetup = (props: WizardStepperProps) => {
     const { experiment, setErrorMessage } = props;
+    const [ loading, setLoading ] = useState(false);
     const [ notebookUp, setNotebookUp ] = useState(false);
     const [ notebookPath, setNotebookPath ] = useState('');
 
     const getNotebook = async () => {
+        setLoading(true);
         const { data, error } = await get_notebook(experiment.id);
         setErrorMessage(error || '');
-        setNotebookUp(data?.status === 'up');
+        setNotebookUp(data?.message === 'up');
         setNotebookPath(data?.path || '');
+        setLoading(false);
     }
 
     const spawnNotebook = async () => {
         const { error } = await spawn_notebook(experiment.id);
         setErrorMessage(error || '');
+        getNotebook();
     }
 
     const deleteNotebook = async () => {
         const { error } = await delete_notebook(experiment.id);
         setErrorMessage(error || '');
+        getNotebook();
     }
 
     useEffect(() => {
@@ -33,10 +38,25 @@ const WizardSetup = (props: WizardStepperProps) => {
 
     return (
         <Box sx={{ p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Typography variant="h5">Notebook status: {notebookUp ? 'up' : 'down'}</Typography>
-            <Button variant="contained" color="primary" onClick={spawnNotebook}>Spawn Jupyter Notebook</Button>
-            <Button variant="contained" color="success" disabled={!notebookUp} href={notebookPath}>Open Jupyter Notebook</Button>
-            <Button variant="contained" color="error" onClick={deleteNotebook}>Delete Jupyter Notebook</Button>
+
+            { loading && (
+                <CircularProgress />
+            ) || (
+                <Stack spacing={2} direction="column">
+                    {notebookUp && (
+                        <>
+                            <Typography variant="h5">Notebook running 🚀</Typography>
+                            <Button variant="contained" color="success" href={notebookPath} target='_blank'>Open Jupyter Notebook</Button>
+                            <Button variant="contained" color="error" onClick={deleteNotebook}>Delete Jupyter Notebook</Button>
+                        </>
+                    ) || (
+                        <>
+                            <Typography variant="h5">Notebook down 💔</Typography>
+                            <Button variant="contained" color="primary" onClick={spawnNotebook}>Spawn Jupyter Notebook</Button>
+                        </>
+                    )}
+                </Stack>
+            )}
         </Box>
     )
 }
