383561353a3bc86243673b98ea0545adb1bc687c
Filip Krása <fidasek009@gmail.com>
2025-03-13 12:41:48 +0100

    add demo API for easier frontend development and debugging

--- dashboard/dash/src/util/const.ts
+++ dashboard/dash/src/util/const.ts
@@ -1,6 +1,7 @@
 
 export const BASE_PATH = '/__BASE_PATH__';
 export const API_BASE = '/__API_PATH__';
+// export const API_BASE = 'http://localhost:8888/api';    // when running the _demo.py API server
 
 export const HUB_API_BASE = '/hub/api';
 
