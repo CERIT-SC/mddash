bbcc7130fa18a3b48b385b3787742efe70940e9a
Filip Krása <xkrasa@fi.muni.cz>
2025-04-08 12:02:30 +0200

    add tuner demo

---
+++ dashboard/api/tuner-client.py
@@ -0,0 +1,32 @@
+import requests
+from pathlib import Path
+
+
+TUNER_URL = 'https://gromacs-tuner.dyn.cloud.e-infra.cz/api'
+
+
+def run_submit(tpr_path: Path) -> dict:
+    '''
+    Submit an experiment to the tuner.
+
+    :param experiment_id: The ID of the experiment to submit.
+    :return: The response from the tuner.
+    '''
+    with open(tpr_path, 'rb') as f:
+        files = {'file': f}
+        response = requests.post(f'{TUNER_URL}/tuner_runs', files=files)
+
+    response.raise_for_status()
+    return response.json()
+
+
+def poll_status(run_id: str) -> dict:
+    '''
+    Poll the status of a submitted job.
+
+    :param run_id: The ID of the job to poll.
+    :return: The response from the tuner.
+    '''
+    response = requests.get(f'{TUNER_URL}/tuner_runs/{run_id}/status')
+    response.raise_for_status()
+    return response.json()
--- dashboard/dash/src/components/Wizard/Tune.tsx
+++ dashboard/dash/src/components/Wizard/Tune.tsx
@@ -1,13 +1,59 @@
+import { useEffect, useState } from 'react';
+import { Box, Stack, Button, Typography, CircularProgress } from '@mui/material'
 
 import { WizardStepperProps } from "./Stepper"
+import { tuner_status, run_tuner } from '../../util/api';
 
 const WizardTune = (props: WizardStepperProps) => {
-    console.log(props);
+    const { experiment, setErrorMessage } = props;
+    const [ loading, setLoading ] = useState(false);
+    const [ tunerUp, setTunerUp ] = useState(false);
+    const [ tunerStatus, setTunerStatus ] = useState({});
+
+
+    console.log(experiment);
+    console.log(setErrorMessage);
+
+    const getTuner = async () => {
+        setLoading(true);
+        const { data, error } = await tuner_status(experiment.id);
+        setErrorMessage(error || '');
+        setTunerUp(data?.message === 'up');
+        setTunerStatus(data?.status || {});
+        setLoading(false);
+    }
+
+    const runTuner = async () => {
+        const { error } = await run_tuner(experiment.id);
+        setErrorMessage(error || '');
+        getTuner();
+    }
+
+    useEffect(() => {
+        getTuner();
+    }, []);
 
     return (
-        <div>
-            <h1>TODO: Tune</h1>
-        </div>
+        <Box sx={{ p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+
+            { loading && (
+                <CircularProgress />
+            ) || (
+                <Stack spacing={2} direction="column">
+                    {tunerUp && (
+                        <>
+                            <Typography variant="h5">Tuner running 🚀</Typography>
+                            <pre>{ JSON.stringify(tunerStatus, null, 4) }</pre>
+                        </>
+                    ) || (
+                        <>
+                            <Typography variant="h5">Tuner isn't running 💔</Typography>
+                            <Button variant="contained" color="primary" onClick={runTuner}>Run tuner</Button>
+                        </>
+                    )}
+                </Stack>
+            )}
+        </Box>
     );
 }
 
