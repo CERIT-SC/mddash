4d8fac6bf97269e567e86b6fd7df77113e31c398
Filip Krása <xkrasa@fi.muni.cz>
2025-06-03 12:39:10 +0200

    add molstar

--- dashboard/dash/src/components/Wizard/Analyze.tsx
+++ dashboard/dash/src/components/Wizard/Analyze.tsx
@@ -1,14 +1,56 @@
+import { useEffect } from "react";
 
-import { WizardStepperProps } from "./Stepper"
+import { Stack } from "@mui/material";
+
+import { WizardStepperProps } from "./Stepper";
+import { initViewerUI, loadStructure } from "../MolStar";
+
+const CONTAINER_NAME = "molstar-container";
 
 const WizardAnalyze = (props: WizardStepperProps) => {
     console.log(props);
 
-    return (
-        <div>
-            <h1>TODO: Analyze</h1>
-        </div>
-    )
-}
+    async function init() {
+        const plugin = await initViewerUI(CONTAINER_NAME, {
+            spec: {
+                layout: {
+                    initial: {
+                        isExpanded: false,
+                        showControls: false,
+                        controlsDisplay: "reactive",
+                    },
+                },
+                behaviors: [],
+            },
+        });
 
-export default WizardAnalyze
+        // TODO: load actual file from experiment
+        await loadStructure(plugin, "https://models.rcsb.org/4hhb.bcif", { isBinary: true });
+    }
+
+    useEffect(() => {
+        init().catch((error) => {
+            console.error("Error initializing Mol* viewer:", error);
+        });
+    }, []);
+
+    return (
+        <Stack
+            alignContent={"center"}
+            justifyContent="center"
+            alignItems="center"
+            sx={{ width: "100%", height: "100%" }}
+        >
+            <div
+                id={CONTAINER_NAME}
+                style={{
+                    width: "800px",
+                    height: "600px",
+                    position: "relative",
+                }}
+            />
+        </Stack>
+    );
+};
+
+export default WizardAnalyze;
