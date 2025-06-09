db4458953c00307bd057e430813458d7158541f0
Filip Krása <xkrasa@fi.muni.cz>
2025-06-09 15:49:07 +0200

    improve molstar usability

--- dashboard/dash/src/components/Wizard/Analyze.tsx
+++ dashboard/dash/src/components/Wizard/Analyze.tsx
@@ -1,56 +1,29 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
-import { Stack } from "@mui/material";
+import { Stack, CircularProgress } from "@mui/material";
 
 import { WizardStepperProps } from "./Stepper";
-import { initViewerUI, loadStructure } from "../MolStar";
+import MolStar from "../MolStar";
 
-const CONTAINER_NAME = "molstar-container";
 
 const WizardAnalyze = (props: WizardStepperProps) => {
-    console.log(props);
-
-    async function init() {
-        const plugin = await initViewerUI(CONTAINER_NAME, {
-            spec: {
-                layout: {
-                    initial: {
-                        isExpanded: false,
-                        showControls: false,
-                        controlsDisplay: "reactive",
-                    },
-                },
-                behaviors: [],
-            },
-        });
-
-        // TODO: load actual file from experiment
-        await loadStructure(plugin, "https://models.rcsb.org/4hhb.bcif", { isBinary: true });
-    }
+    const [molstarComponent, setMolstarComponent] = useState<JSX.Element | null>(null);
 
     useEffect(() => {
-        init().catch((error) => {
-            console.error("Error initializing Mol* viewer:", error);
-        });
+        const molstar = <MolStar width="800px" height="600px" pdbId="4A8B" />;
+        setMolstarComponent(molstar);
     }, []);
 
     return (
-        <Stack
-            alignContent={"center"}
-            justifyContent="center"
-            alignItems="center"
-            sx={{ width: "100%", height: "100%" }}
-        >
-            <div
-                id={CONTAINER_NAME}
-                style={{
-                    width: "800px",
-                    height: "600px",
-                    position: "relative",
-                }}
-            />
+        <Stack alignItems="center">
+            {!molstarComponent ? (
+                <CircularProgress />
+            ) : (
+                molstarComponent
+            )}
         </Stack>
     );
 };
+
 
 export default WizardAnalyze;
