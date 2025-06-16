1d6e686e68a2c633bfd58b6b50b3befdd6f6cc89
Filip Krása <xkrasa@fi.muni.cz>
2025-06-16 19:27:00 +0200

    unify react code style

--- dashboard/dash/src/components/Wizard/Stepper.tsx
+++ dashboard/dash/src/components/Wizard/Stepper.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
-import { styled, Stepper, Step, StepLabel, StepIconProps, StepConnector, stepConnectorClasses, Box, Button } from '@mui/material';
-import { BlurOn, Tune, PlayArrow, Assessment, Publish } from '@mui/icons-material';
+import {
+    styled,
+    Stepper,
+    Step,
+    StepLabel,
+    StepIconProps,
+    StepConnector,
+    stepConnectorClasses,
+    Box,
+    Button,
+} from "@mui/material";
+import { BlurOn, Tune, PlayArrow, Assessment, Publish } from "@mui/icons-material";
 
 import WizardSetup from "./Setup";
 import WizardTune from "./Tune";
@@ -10,11 +20,11 @@ import WizardPublish from "./Publish";
 import { Experiment } from "../../util/types";
 
 const steps = [
-    { label: 'Setup', icon: <BlurOn />, child: WizardSetup },
-    { label: 'Tune', icon: <Tune />, child: WizardTune },
-    { label: 'Run', icon: <PlayArrow />, child: WizardRun },
-    { label: 'Analyze', icon: <Assessment />, child: WizardAnalyze },
-    { label: 'Publish', icon: <Publish />, child: WizardPublish },
+    { label: "Setup", icon: <BlurOn />, child: WizardSetup },
+    { label: "Tune", icon: <Tune />, child: WizardTune },
+    { label: "Run", icon: <PlayArrow />, child: WizardRun },
+    { label: "Analyze", icon: <Assessment />, child: WizardAnalyze },
+    { label: "Publish", icon: <Publish />, child: WizardPublish },
 ];
 
 const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
@@ -23,50 +33,48 @@ const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
     },
     [`&.${stepConnectorClasses.active}`]: {
         [`& .${stepConnectorClasses.line}`]: {
-            backgroundImage:
-                'linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)',
+            backgroundImage: "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)",
         },
     },
     [`&.${stepConnectorClasses.completed}`]: {
         [`& .${stepConnectorClasses.line}`]: {
-            backgroundImage:
-                'linear-gradient( 95deg,rgb(0,200,83) 0%,rgb(0,150,136) 50%,rgb(0,100,83) 100%)',
+            backgroundImage: "linear-gradient( 95deg,rgb(0,200,83) 0%,rgb(0,150,136) 50%,rgb(0,100,83) 100%)",
         },
     },
     [`& .${stepConnectorClasses.line}`]: {
         height: 3,
         border: 0,
-        backgroundColor: '#eaeaf0',
+        backgroundColor: "#eaeaf0",
         borderRadius: 1,
-        ...theme.applyStyles('dark', {
+        ...theme.applyStyles("dark", {
             backgroundColor: theme.palette.grey[800],
         }),
     },
 }));
 
-const ColorlibStepIconRoot = styled('div')<{ownerState: { completed?: boolean; active?: boolean }}>(({ theme, ownerState }) => ({
-    backgroundColor: '#ccc',
-    zIndex: 1,
-    color: '#fff',
-    width: 50,
-    height: 50,
-    display: 'flex',
-    borderRadius: '50%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    ...theme.applyStyles('dark', {
-        backgroundColor: theme.palette.grey[700],
-    }),
-    ...(ownerState.active && {
-        backgroundImage:
-            'linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)',
-        boxShadow: '0 4px 10px 0 rgba(0,0,0,.25)',
-    }),
-    ...(ownerState.completed && {
-        backgroundImage:
-            'linear-gradient( 136deg, rgb(0,200,83) 0%, rgb(0,150,136) 50%, rgb(0,100,83) 100%)',
-    }),
-}));
+const ColorlibStepIconRoot = styled("div")<{ ownerState: { completed?: boolean; active?: boolean } }>(
+    ({ theme, ownerState }) => ({
+        backgroundColor: "#ccc",
+        zIndex: 1,
+        color: "#fff",
+        width: 50,
+        height: 50,
+        display: "flex",
+        borderRadius: "50%",
+        justifyContent: "center",
+        alignItems: "center",
+        ...theme.applyStyles("dark", {
+            backgroundColor: theme.palette.grey[700],
+        }),
+        ...(ownerState.active && {
+            backgroundImage: "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+            boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)",
+        }),
+        ...(ownerState.completed && {
+            backgroundImage: "linear-gradient( 136deg, rgb(0,200,83) 0%, rgb(0,150,136) 50%, rgb(0,100,83) 100%)",
+        }),
+    })
+);
 
 export interface WizardStepperProps {
     experiment: Experiment;
@@ -74,7 +82,7 @@ export interface WizardStepperProps {
     setErrorMessage: (message: string) => void;
 }
 
-export default function WizardStepper(props: WizardStepperProps) {
+const WizardStepper = (props: WizardStepperProps) => {
     const { experiment, setExperiment } = props;
     const [activeStep, setActiveStep] = useState(Math.min(experiment.step, steps.length - 1));
 
@@ -83,30 +91,38 @@ export default function WizardStepper(props: WizardStepperProps) {
         if (step > experiment.step) return;
 
         setActiveStep(step);
-    }
+    };
 
     const nextStep = () => {
         if (experiment.step >= steps.length - 1) return;
 
         setActiveStep(experiment.step + 1);
-        setExperiment((prev: Experiment) => {return { ...prev, step: prev.step + 1 }});
-    }
+        setExperiment((prev: Experiment) => {
+            return { ...prev, step: prev.step + 1 };
+        });
+    };
 
     const ColorlibStepIcon = (props: StepIconProps) => {
         const { active, completed, className, icon } = props;
         const idx = Number(icon) - 1;
         const step = steps[idx];
-    
+
         return (
-            <ColorlibStepIconRoot ownerState={{ completed, active }} className={className} onClick={() => changeStep(idx)}>
+            <ColorlibStepIconRoot
+                ownerState={{ completed, active }}
+                className={className}
+                onClick={() => changeStep(idx)}
+            >
                 {step.icon}
             </ColorlibStepIconRoot>
         );
-    }
+    };
 
     return (
         <>
-            <Button variant="contained" onClick={() => nextStep()}>DEBUG: next step</Button>
+            <Button variant="contained" onClick={() => nextStep()}>
+                DEBUG: next step
+            </Button>
 
             <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
                 {steps.map((step, idx) => (
@@ -115,10 +131,10 @@ export default function WizardStepper(props: WizardStepperProps) {
                     </Step>
                 ))}
             </Stepper>
-            
-            <Box sx={{ mt: 4 }} >
-                { React.createElement(steps[activeStep].child, props) }
-            </Box>
+
+            <Box sx={{ mt: 4 }}>{React.createElement(steps[activeStep].child, props)}</Box>
         </>
     );
-}
+};
+
+export default WizardStepper;
