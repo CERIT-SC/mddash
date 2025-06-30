3618072da383ecb5322a9ca52f664ceaa80f6f55
Filip Krása <fidasek009@gmail.com>
2025-06-30 23:08:19 +0200

    make molstar viewer functional

--- dashboard/dash/src/components/Wizard/Analyze.tsx
+++ dashboard/dash/src/components/Wizard/Analyze.tsx
@@ -1,31 +1,48 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
-import { Stack, CircularProgress } from "@mui/material";
+import { Stack } from "@mui/material";
 
 import { WizardStepProps } from "./Stepper";
 import MolStar from "../MolStar";
-
+import FileSelector from "../FileSelector";
 
 const WizardAnalyze = (props: WizardStepProps) => {
-    console.log(props);
+    const { experiment, setErrorMessage } = props;
 
-    const [molstarComponent, setMolstarComponent] = useState<JSX.Element | null>(null);
+    const [structureFile, setStructureFile] = useState<string>("");
+    const [trajectoryFile, setTrajectoryFile] = useState<string>("");
 
-    useEffect(() => {
-        const molstar = <MolStar width="800px" height="600px" pdbId="4A8B" />;
-        setMolstarComponent(molstar);
-    }, []);
+    const structureType = "pdb";
+    const trajectoryType = "xtc";
 
     return (
-        <Stack alignItems="center">
-            {!molstarComponent ? (
-                <CircularProgress />
-            ) : (
-                molstarComponent
+        <Stack alignItems="center" spacing={2}>
+            <FileSelector
+                experimentId={experiment.id}
+                extension={structureType}
+                onFileSelected={setStructureFile}
+                setErrorMessage={setErrorMessage}
+            />
+            <FileSelector
+                experimentId={experiment.id}
+                extension={trajectoryType}
+                onFileSelected={setTrajectoryFile}
+                setErrorMessage={setErrorMessage}
+            />
+
+            {structureFile && (
+                <MolStar
+                    width="800px"
+                    height="600px"
+                    structureUrl={structureFile}
+                    structureFormat={structureType}
+                    trajectoryUrl={trajectoryFile}
+                    trajectoryFormat={trajectoryType}
+                    setErrorMessage={setErrorMessage}
+                />
             )}
         </Stack>
     );
 };
-
 
 export default WizardAnalyze;
