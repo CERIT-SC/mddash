40f4fc235a8ba597be52aa1668054ce4b3096f9c
Filip Krása <fidasek009@gmail.com>
2025-08-23 13:01:48 +0200

    fix deployment

--- mdrun-api/config.py
+++ mdrun-api/config.py
@@ -9,7 +9,7 @@ API_PREFIX = '/api'
 
 DATA_DIR = Path('/data')
 DATA_DIR.mkdir(parents=True, exist_ok=True)
-DB_URL = 'sqlite:///data/mdrun.db'
+DB_URL = f'sqlite:///{DATA_DIR}/mdrun.db'
 
 NAMESPACE = os.environ.get('POD_NAMESPACE', 'default')
 PVC_NAME = os.environ.get('PVC_NAME', 'mdrun-api-pvc')
