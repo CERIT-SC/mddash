185113538e2e73616b46da077f42ffacdfa5fef0
Filip Krása <xkrasa@fi.muni.cz>
2025-10-12 12:54:17 +0200

    add minor improvement to the mdrun-api

--- mdrun-api/app.py
+++ mdrun-api/app.py
@@ -9,21 +9,21 @@ from polling import start_polling
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    
+
     app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
+
     CORS(app)
-    
+
     db.init_app(app)
     ma.init_app(app)
-    
+
     app.register_blueprint(mdrun_bp)
     app.register_blueprint(health_bp)
-    
+
     with app.app_context():
         db.create_all()
-    
+
     return app
 
 
