2876b71ecc440c731d81a80ac624543eabe408c0
Filip Krása <xkrasa@fi.muni.cz>
2025-11-04 10:57:44 +0000

    fix UI demo deployment inside devcontainer

--- dashboard/dash/vite.config.ts
+++ dashboard/dash/vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/__BASE_PATH__/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    base: "/__BASE_PATH__/",
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-})
+
+    // Dev server configuration
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+    },
+});
